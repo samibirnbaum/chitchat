8a5469703b0b0b817e41eb07c276a4e6153ade80
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2017-11-07 18:06:07 +0000

    messages sending and showing

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,6 +1,6 @@
 (function(){
 
-    function Message($firebaseArray){
+    function Message($firebaseArray, $cookies){
         var Message = {}; //unlike a controller which is a constructor function, where you can use this, in a factory function we create and return the object ourselves
 
         //PRIVATE ATTRIBUTES
@@ -10,12 +10,25 @@
         //PUBLIC ATTRIBUTES
         Message.allMessages = messagesAll;
         Message.currentMessages = null; //holds message object
+        Message.roomId = null;
 
         //PUBLIC FUNCTIONS
         Message.getByRoomId = function(roomId){ //a function that will access only those messages associated with a roomId
+            this.roomId = roomId;
             var refQueried = ref.orderByChild("roomId").equalTo(roomId);
             var messagesQueried = $firebaseArray(refQueried);
             this.currentMessages = messagesQueried;
+        };
+
+        Message.sendMessage = function(message){
+            var currentUser = $cookies.get('blocChatCurrentUser');
+            var messageObject = {
+                content: message,
+                sentAt: 03+":"+45,
+                username: currentUser,
+                roomId: Message.roomId
+            };
+            messagesAll.$add(messageObject);
         };
 
         return Message;
@@ -25,6 +38,6 @@
 
     angular
         .module("blocChat")
-        .factory("Message",["$firebaseArray", Message]); //a service to interact with fb
+        .factory("Message",["$firebaseArray", "$cookies", Message]); //a service to interact with fb
 
 })();
