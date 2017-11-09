a506ac222192f852afc3a49a255e68179e82f9b2
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2017-11-09 16:38:22 +0000

    textarea clears after message sent

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -4,6 +4,13 @@
     function HomeCtrl(Room, Message) {
         this.room = Room; //this injects the room service but also puts it on the scope of this controller so can be accessed in the view
         this.message = Message;
+        this.sentMessage = "";
+        
+        this.sendMessage = function(){
+            Message.sendMessage(this.sentMessage);
+            this.sentMessage = "";
+        };
+    
     }
     
     angular
